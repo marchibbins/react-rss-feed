41e77deb088ca481e9765cafbdac54a6243fec41
Marc Hibbins <marchibbins@gmail.com>
2016-10-25 14:28:15 +0100

    Build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: {
+        Demo: "./src/Demo.js",
+        ReactRSS: "./src/ReactRSS.js"
+    },
+    output: {
+        path: "dist",
+        filename: "[name].js"
+    }
+}
