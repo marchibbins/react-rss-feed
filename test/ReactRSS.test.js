386fa6ee470b3fbd95e2c0082836e291aa5cb2f0
Marc Hibbins <marchibbins@gmail.com>
2016-10-25 17:13:50 +0100

    Supply initial test outlines

---
+++ test/ReactRSS.test.js
@@ -0,0 +1,9 @@
+test('ReactRSS requires a URL property');
+
+test('ReactRSS optionally takes a count property');
+
+test('ReactRSS fetches the given URL');
+
+test('ReactRSS renders one <item> feed item');
+
+test('ReactRSS enforces count property');
