af06ae27048e978a48c396ce43be61a66db7f80f
Marc Hibbins <marchibbins@gmail.com>
2016-10-27 11:40:51 +0100

    Separate loader functions

--- src/RSSLoader.js
+++ src/RSSLoader.js
@@ -6,32 +6,32 @@ import 'whatwg-fetch';
 
 ES6Promise.polyfill();
 
-const RSSLoader = {
-    getFeed: url => {
-        return fetch(url)
-            .then(response => {
-                if (response.status >= 200 && response.status < 300) {
-                    return response.text();
-                } else {
-                    let error = new Error(response.statusText);
-                    error.response = response;
-                    throw error;
-                }
-            })
-            .then(xmlString => {
-                return new Promise(function (resolve, reject) {
-                    to_json(xmlString, function (error, json) {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            let items = json.rss.channel.item; // Object not array!
-                            resolve(Object.keys(items).map(key => items[key]));
-                        }
-                    });
-               });
-            });
-    }
-
+const fetchFeed = url => {
+    return fetch(url)
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response.text();
+            } else {
+                let error = new Error(response.statusText);
+                error.response = response;
+                throw error;
+            }
+        });
 };
 
-export default RSSLoader;
+const parseFeed = xmlString => {
+    return new Promise(function (resolve, reject) {
+        to_json(xmlString, function (error, json) {
+            if (error) {
+                reject(error);
+            } else {
+                let items = json.rss.channel.item; // Object not array!
+                resolve(Object.keys(items).map(key => items[key]));
+            }
+        });
+   });
+};
+
+export default {
+    getFeed: url => fetchFeed(url).then(parseFeed)
+};
