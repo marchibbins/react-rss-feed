056db42f97b9d9c3800a26d01d174bc6bd41e84f
Marc Hibbins <marchibbins@gmail.com>
2016-10-27 11:35:23 +0100

    Outline tests for loader

---
+++ test/RSSLoader.test.js
@@ -0,0 +1,17 @@
+describe('RSSLoader', () => {
+
+    it('attempts to fetch the given URL');
+
+    it('attempts to pase XML string to JSON');
+
+    it('getFeed returns a Promise');
+
+    it('getFeed throws an error on bad attempt');
+
+    it('parseFeed returns a Promise');
+
+    it('parseFeed throws an error on bad attempt');
+
+    it('uses polyfill for Promises');
+
+});
