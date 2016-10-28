e7452dca3c39367ba7e6089f1c6db26657d5b35a
Marc Hibbins <marchibbins@gmail.com>
2016-10-28 14:49:27 +0100

    Promise loader tests

--- test/RSSLoader.test.js
+++ test/RSSLoader.test.js
@@ -1,16 +1,40 @@
-describe('RSSLoader', () => {
+import { fetchFeed, parseFeed } from '../src/RSSLoader';
+
+describe('RSSLoader component', () => {
 
     it('attempts to fetch the given URL');
 
     it('attempts to pase XML string to JSON');
 
-    it('getFeed returns a Promise');
+    it('getFeed returns a Promise', () => {
+        const func = fetchFeed.call();
+        expect(func).toBeInstanceOf(Promise);
+        expect(func.then).toBeInstanceOf(Function);
+    });
 
-    it('getFeed throws an error on bad attempt');
+    it('getFeed rejects on bad attempt');
 
-    it('parseFeed returns a Promise');
+    it('fetchFeed returns a Promise', () => {
+        const func = fetchFeed.call();
+        expect(func).toBeInstanceOf(Promise);
+        expect(func.then).toBeInstanceOf(Function);
+    });
 
-    it('parseFeed throws an error on bad attempt');
+    it('fetchFeed rejects on bad attempt');
+
+    it('parseFeed returns a Promise', () => {
+        const func = parseFeed.call();
+        expect(func).toBeInstanceOf(Promise);
+        expect(func.then).toBeInstanceOf(Function);
+    });
+
+    it('parseFeed rejects on bad attempt', () => {
+        return parseFeed('badXML').then(() => {
+            throw new Error('Promise should be rejected');
+        }, (error) => {
+            expect(error).toBeInstanceOf(Error);
+        });
+    });
 
     it('uses polyfill for Promises');
 
